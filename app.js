3270d8ef3e6450ec9169f63f948ae5dc052d9845
jjoaovitorf <155121553+jjoaovitorf@users.noreply.github.com>
2024-11-09 12:13:22 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var produtoImg = document.getElementById("produtoImg");
+var produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
+produtoMiniatura[0].onclick = function(){
+    produtoImg.src = produtoMiniatura[0].src;
+}
+produtoMiniatura[1].onclick = function(){
+    produtoImg.src = produtoMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function(){
+    produtoImg.src = produtoMiniatura[2].src;
+}
+
+produtoMiniatura[3].onclick = function(){
+    produtoImg.src = produtoMiniatura[3].src;
+}
