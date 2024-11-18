6ee094fc2fbe34220701b2cc558a387629149e2f
jjoaovitorf <155121553+jjoaovitorf@users.noreply.github.com>
2024-11-17 21:30:23 -0300

    Update loja.sql

--- loja.sql
+++ loja.sql
@@ -8,3 +8,17 @@ CREATE TABLE IF NOT EXISTS usuarios (
     email VARCHAR(255) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL
 );
+
+
+CREATE TABLE estoque (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    quantidade INT NOT NULL DEFAULT 0
+);
+
+
+ALTER TABLE usuarios ADD COLUMN is_admin TINYINT(1) DEFAULT 0;
+
+UPDATE usuarios 
+SET is_admin = 1 
+WHERE email = 'admin@example.com';
