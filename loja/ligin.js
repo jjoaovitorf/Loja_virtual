ec293671ca2475fc0b5b6e2b948b64edc6bc6d1d
jjoaovitorf <155121553+jjoaovitorf@users.noreply.github.com>
2024-11-25 22:26:48 -0300

    Update login.js

--- loja/ligin.js
+++ loja/ligin.js
@@ -5,7 +5,6 @@ function mostrarEntrar() {
     document.getElementById("btnCadastro").classList.remove("active");
     document.getElementById("Indicador").style.transform = "translateX(25px)";
 }
-
 function mostrarCadastro() {
     document.getElementById("EntrarPainel").classList.remove("active");
     document.getElementById("CadastroSite").classList.add("active");
